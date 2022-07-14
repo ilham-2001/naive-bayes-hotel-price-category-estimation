ca35bbf47dac5e9ebce8e1a5b1e812238244bb8d
ilham-2001 <arizqyakbar@gmail.com>
2022-07-14 21:32:11 +0700

    add

--- webscrap.py
+++ webscrap.py
@@ -1,4 +1,3 @@
-from regex import B
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,7 +8,7 @@ import time
 import pandas as pd
 import numpy as np
 
-url = "https://www.google.com/travel/hotels?q=harga%20hotel%20di%20jakarta&gsas=1&rp=CgpYAGAAcgQIAhgAOAGKAhZoYXJnYSBob3RlbCBkaSBqYWthcnRhqAIA&ved=0CAAQ5JsGahcKEwjYudLgwff4AhUAAAAAHQAAAAAQAw&hl=en-ID&gl=id&g2lb=2502548%2C2503771%2C2503781%2C4258168%2C4270442%2C4284970%2C4291517%2C4306835%2C4308226%2C4515404%2C4597339%2C4649665%2C4703207%2C4718358%2C4722900%2C4723331%2C4741665%2C4757164%2C4758493%2C4762561%2C4779784%2C4786958%2C4787395%2C4790928%2C4794648&utm_campaign=sharing&utm_medium=link&utm_source=htls&ts=CAESCgoCCAMKAggDEAAaKwoNEgk6B0pha2FydGEaABIaEhQKBwjmDxAHGBsSBwjmDxAHGBwYATICEAAqEQoNEgQCAwQFKAE6A0lEUhoA&ap=MAFapwMKBgiA6jAQACIDSURSKhYKBwjmDxAHGBsSBwjmDxAHGBwYASgAOAI4BDgFsAEAWAFoAXIECAIYAJoBCRIHSmFrYXJ0YaIBEwoIL20vMDQ0cnYSB0pha2FydGGqAS8KAgghEgIICBICCGUSAggVEgIIDRICCGcSAghbEgIILxICCFoSAwiMAhICCCAYAaoBBwoDCOUBGACqARMKAggSEgMImwESAghoEgIIaRgBqgEHCgMI4QEYAKoBBwoDCKECGACqAQcKAwjkARgAqgEOCgIIHBICCFESAghHGAGqAQcKAwjiARgAqgETCgIIJRIDCNsCEgIIdRICCHYYAaoBEgoCCBESAghAEgIIOBICCAIYAaoBBwoDCMUCGACqAS0KAgguEgIIPBIDCIcBEgIIGhICCEgSAwiBAhICCAMSAggMEgMIjwESAggnGAGqAQwKAwiuARIDCK8BGAGqAQsKAwjhAhICCGMYAaoBCwoCCDUSAwiWARgBkgICCA6SAgIIEJICAggTkgICCA-SAgIIEpICAggRkgICCA2SAgIIDJICAggUkgECIAE"
+url = "https://www.google.com/travel/hotels?q=harga%20hotel%20di%20jakarta&gsas=1&rp=CgpYAGAAcgQIAhgAOAGKAhZoYXJnYSBob3RlbCBkaSBqYWthcnRhqAIA&ved=0CAAQ5JsGahcKEwiIgpq1r_j4AhUAAAAAHQAAAAAQBA&hl=en-ID&gl=id&g2lb=2502548%2C2503771%2C2503781%2C4258168%2C4270442%2C4284970%2C4291517%2C4306835%2C4308226%2C4515404%2C4597339%2C4649665%2C4703207%2C4718358%2C4722900%2C4723331%2C4741665%2C4757164%2C4758493%2C4762561%2C4779784%2C4786958%2C4787395%2C4790928%2C4794648&utm_campaign=sharing&utm_medium=link&utm_source=htls&ts=CAESCgoCCAMKAggDEAAaKwoNEgk6B0pha2FydGEaABIaEhQKBwjmDxAHGBsSBwjmDxAHGBwYATICEAAqDgoKEgECKAE6A0lEUhoA&ap=MAFapQMKBgiA6jAQACIDSURSKhYKBwjmDxAHGBsSBwjmDxAHGBwYASgAOAI4A7ABAFgBaAFyBAgCGACaAQkSB0pha2FydGGiARMKCC9tLzA0NHJ2EgdKYWthcnRhqgEvCgIIIRICCAgSAghlEgIIFRICCA0SAghnEgIIWxICCC8SAghaEgMIjAISAgggGAGqAQcKAwjlARgAqgETCgIIEhIDCJsBEgIIaBICCGkYAaoBBwoDCOEBGACqAQcKAwihAhgAqgEHCgMI5AEYAKoBDgoCCBwSAghREgIIRxgBqgEHCgMI4gEYAKoBEwoCCCUSAwjbAhICCHUSAgh2GAGqARIKAggREgIIQBICCDgSAggCGAGqAQcKAwjFAhgAqgEtCgIILhICCDwSAwiHARICCBoSAghIEgMIgQISAggDEgIIDBIDCI8BEgIIJxgBqgEMCgMIrgESAwivARgBqgELCgMI4QISAghjGAGqAQsKAgg1EgMIlgEYAZICAggSkgICCBGSAgIIDpICAggTkgICCAySAgIIEJICAggPkgICCA2SAgIIFJIBAiAB"
 xpath = "/html/body/c-wiz[2]/div/div[2]/div/c-wiz/div[2]/div[2]/div[1]/div/main/div/c-wiz/div[1]/div[4]/c-wiz[5]/c-wiz/div/div/div/div[1]/div/div[1]/div[1]/div[1]/h2"
 
 ID = "id"
@@ -27,9 +26,6 @@ class WebScrapper:
     def __init__(self):
         self.driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()))
-
-        # header = self.driver.find_elements(By.CLASS_NAME, "BgYkof")
-        # div = self.driver.find_elements(By.CLASS_NAME, "kixHKb")
 
     def GetUrl(self, url):
         self.driver.get(url)
@@ -57,7 +53,7 @@ if __name__ == "__main__":
     scrapper = WebScrapper()
     scrapper.GetUrl(url)
 
-    time.sleep(120)
+    time.sleep(45)
     hotel = []
 
     cards = scrapper.getElements(By.CLASS_NAME, "pjDrrc")
@@ -103,46 +99,45 @@ if __name__ == "__main__":
             user_rate = "-"
             rater_count = "-"
 
-        # bottom side ED0Ckc
+        # bottom side
         bottom_lhs1 = lhs1.find_element(By.CLASS_NAME, "RJM8Kc")
         bottom_lhs2 = bottom_lhs1.find_element(By.CLASS_NAME, "ED0Ckc")
-        bottom_lhs3 = bottom_lhs2.find_element(By.CLASS_NAME, "HlxIlc")
-        fasilitas = bottom_lhs3.find_elements(By.TAG_NAME, "li")
 
-        facility = []
+        try:
 
-        for fasil in fasilitas:
-            facility.append(fasil.text)
+            bottom_lhs3 = bottom_lhs2.find_element(By.CLASS_NAME, "HlxIlc")
+            fasilitas = bottom_lhs3.find_elements(By.TAG_NAME, "li")
 
-         # mencoba untuk mengambil bintang hotel
-        hotel_class = bottom_lhs1.find_element(By.CLASS_NAME, "UqrZme")
-        # print(hotel_class1.text)
+            facility = []
+
+            for fasil in fasilitas:
+                facility.append(fasil.text)
+
+        except Exception:
+            facility = []
+
+        try:
+            hotel_class = bottom_lhs1.find_element(By.CLASS_NAME, "UqrZme")
+
+            # mencoba untuk mengambil bintang hotel
+            hotel_class = hotel_class.text
+
+        except Exception:
+            hotel_class = "-"
 
         # right hand side
         rhs1 = element3.find_element(By.CLASS_NAME, "PwV1Ac")
         try:
             rhs2 = rhs1.find_element(By.CLASS_NAME, "OxGZuc")
-            harga = rhs2.find_element(By.TAG_NAME, "span")
+            rhs3 = rhs2.find_element(By.CLASS_NAME, "Q01V4b")
+            rhs4 = rhs3.find_element(By.TAG_NAME, "span")
+            harga = rhs4.find_element(By.TAG_NAME, "span")
             harga = harga.text
         except Exception:
             harga = 0
 
-        hotel.append((nama_hotel.text, hotel_class.text, user_rate,
+        hotel.append((nama_hotel.text, hotel_class, user_rate,
                      rater_count, str(facility), harga))
-
-    # for i, d in enumerate(div):
-    #     val = d.find_element(By.TAG_NAME, "span")
-    #     if "IDR" in val.text.split():
-    #         print(val.text)
-    #         _, mun = val.text.split()
-
-    #         harga_hotel.append(int(mun.replace(",", "")))
 
     arr = np.asarray(hotel)
     scrapper.toCSV(arr)
-
-    # data = scrapper.getElements(By.CLASS_NAME, "XrXoS")
-
-
-# J2W8oe osw6J CiuVaf untuk nama dan review user
-# J2W8oe PwV1Ac BIjKEe untuk harga
